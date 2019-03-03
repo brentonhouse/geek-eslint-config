d24a2ac6987cf902edab640d3c090099d3d75251
Brenton House <brenton.house@gmail.com>
2019-03-02 22:42:02 -0600

    adding jsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -130,6 +130,25 @@ module.exports = {
 				}
 			}
 		],
+        "jsdoc/check-examples": "warn",
+        "jsdoc/check-param-names": "warn",
+        "jsdoc/check-tag-names": "warn",
+        "jsdoc/check-types": "warn",
+        "jsdoc/newline-after-description": "warn",
+        "jsdoc/no-undefined-types": "warn",
+        "jsdoc/require-description": "warn",
+        "jsdoc/require-description-complete-sentence": "warn",
+        "jsdoc/require-example": "warn",
+        "jsdoc/require-hyphen-before-param-description": "warn",
+        "jsdoc/require-param": "warn",
+        "jsdoc/require-param-description": "warn",
+        "jsdoc/require-param-name": "warn",
+        "jsdoc/require-param-type": "warn",
+        "jsdoc/require-returns": "warn",
+        "jsdoc/require-returns-check": "warn",
+        "jsdoc/require-returns-description": "warn",
+        "jsdoc/require-returns-type": "warn",
+        "jsdoc/valid-types": "warn",
 		"jsx-quotes": "error",
 		"key-spacing": [
 			"error",
@@ -373,22 +392,23 @@ module.exports = {
 		],
 		"switch-colon-spacing": "error",
 		"template-curly-spacing": "error",
-		"valid-jsdoc": [
-			"warn",
-			{
-				"prefer": {
-					"arg": "param",
-					"argument": "param",
-					"return": "returns"
-				},
-				"preferType": {
-					"Boolean": "boolean",
-					"Number": "number",
-					"Object": "object",
-					"String": "string"
-				}
-			}
-		],
+
+		// "valid-jsdoc": [
+		// 	"warn",
+		// 	{
+		// 		"prefer": {
+		// 			"arg": "param",
+		// 			"argument": "param",
+		// 			"return": "returns"
+		// 		},
+		// 		"preferType": {
+		// 			"Boolean": "boolean",
+		// 			"Number": "number",
+		// 			"Object": "object",
+		// 			"String": "string"
+		// 		}
+		// 	}
+		// ],
 		"valid-typeof": "error",
 		"vars-on-top": "off",
 		"wrap-iife": "error",
